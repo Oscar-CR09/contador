0e244a3e17c2b3857bc75375dba3493c7b850f9f
Oscar <oscarcr189@outlook.com>
2024-01-18 20:14:57 -0600

    Escribiendo contador

--- contador-app/src/ConterApp.jsx
+++ contador-app/src/ConterApp.jsx
@@ -1,13 +1,17 @@
 export const ConterApp = ()=>{
 
+    let counter = 0;
     const counterincrement =()=>{
-        console.log('click')
+        counter = counter + 1;
+        console.log('click' + counter)
     }
     
     return <>
 
+        <h2>El valor del contador { counter } </h2>
         <button onClick= {() => {counterincrement()}} >Incrementar Contador +1 </button>
 
     </>
+
 
 }
